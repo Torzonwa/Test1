58c15d9abcecbb75109cbbbc871da5bbbfbcfebe
William Torzon <torzonwa@grace.edu>
2020-09-02 15:26:47 -0400

    correction

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -7,7 +7,7 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
             Console.WriteLine("Miles per gallon");
-            double mgp = 500 / 27.0;
+            double mpg = 500 / 27.0;
             Console.WriteLine("you got " + mpg + " mpg.");
         }
     }
